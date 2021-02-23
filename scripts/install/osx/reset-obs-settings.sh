67e896d8fc01f93dce79c158ea316dee220ecca1
SCG82 <scg082+github@gmail.com>
2021-02-23 12:26:17 -0800

    mac: update uninstaller package

--- scripts/install/osx/reset-obs-settings.sh
+++ scripts/install/osx/reset-obs-settings.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 rm -rf "$DSTVOLUME/Library/Application Support/obs-studio"
 rm -rf "$HOME/Library/Application Support/obs-studio"
+rm -rf "$DSTVOLUME/Library/CoreMediaIO/Plug-Ins/DAL/obs-mac-virtualcam.plugin"
+rm -rf "$HOME/Library/Caches/com.obsproject.obs-studio"
+rm -f "$HOME/Library/Preferences/com.obsproject.obs-studio.plist"
+rm -f "$HOME/Library/Preferences/com.obsproject.obs-studio.helper.renderer.plist"
+rm -rf "$HOME/Library/Saved Application State/com.obsproject.obs-studio.savedState"
